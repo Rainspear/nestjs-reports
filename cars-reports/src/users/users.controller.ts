1bf8ee5cf73207e5bf6ac5c31a11f68056e1bff0
Khang Phan <khang.phan@opswat.com>
2022-08-15 12:03:37 +0700

    add dto and user service

--- cars-reports/src/users/users.controller.ts
+++ cars-reports/src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
 
-@Controller('users')
-export class UsersController {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    console.log("users-body", body);
+    this.usersService.createUser(body.email, body.password);
+  }
+
+  // @Post('/singin')
+  // login(@Body body: ) {
+
+  // }
+}
+ 
